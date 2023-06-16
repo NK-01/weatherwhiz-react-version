d6ec109161f570b8a9ed404121be68e134f270f4
Nidhi Kumari <72823658+NK-01@users.noreply.github.com>
2023-06-17 03:12:57 +0530

    Added Github link and profile link to the project by creating FooterLinks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
+import FooterLinks from "./FooterLinks";
+
 function App() {
   return (
     <div className="App">
       <Weather />
+      <FooterLinks />
     </div>
   );
 }
---
+++ src/FooterLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function FooterLinks() {
+  return (
+    <div className="FooterLinks">
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/NK-01" target="_blank" rel="noreferrer">
+          Nidhi Kumari
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NK-01/weatherwhiz-react-version"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </p>
+    </div>
+  );
+}
